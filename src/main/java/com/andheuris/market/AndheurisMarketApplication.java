0ace0d8c7155e04bd0be7a5010b1293a48470b8e
Junior Aguilera <junior.aguilera.alvarado@gmail.com>
2020-11-04 22:22:02 -0500

    API version 1

---
+++ src/main/java/com/andheuris/market/AndheurisMarketApplication.java
@@ -0,0 +1,13 @@
+package com.andheuris.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndheurisMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndheurisMarketApplication.class, args);
+	}
+
+}
